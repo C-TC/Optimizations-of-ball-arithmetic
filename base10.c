5dc066d6a14e0704c362d5bf9f75c5882cf7629c
Ran Liao <raliao@student.ethz.ch>
2021-03-10 01:03:49 +0100

    karatsuba algorithm

--- base10.c
+++ base10.c
@@ -409,7 +409,7 @@ int biginteger_div(BigInteger* p_biginteger1, BigInteger* p_biginteger2, BigInte
 //         }
 //     }
 
-//     return 0;
+    return 0;
 }
 
 // int exactfloat_delete(ExactFloat* p_exactfloat){
@@ -454,6 +454,73 @@ int biginteger_div(BigInteger* p_biginteger1, BigInteger* p_biginteger2, BigInte
 //     biginteger_delete(tmp2);
 //     return p_exactfloat_result;
 // }
+
+// The second argument of the split_at function specifies 
+// the number of digits to extract from the right
+// for example, split_at("12345", 3) will extract the 3 final digits, giving: high="12", low="345".
+int biginteger_split(BigInteger* p_biginteger, int m, BigInteger* high, BigInteger* low){
+    size_t high_size = p_biginteger->array_size - m;
+    size_t low_size = m;
+    high->array_size = high_size;
+    low->array_size = low_size;
+    high->array = malloc(sizeof(int) * high_size);
+    low->array = malloc(sizeof(int) * low_size);
+    high->is_positive = p_biginteger->is_positive;    
+    low->is_positive = p_biginteger->is_positive;    
+    memcpy(high->array, p_biginteger->array + low_size, high_size * sizeof(int));
+    memcpy(low->array, p_biginteger->array, low_size * sizeof(int));
+    return 0;
+}
+
+// https://en.wikipedia.org/wiki/Karatsuba_algorithm
+BigInteger* karatsuba(BigInteger* p_biginteger1, BigInteger* p_biginteger2){
+    if(p_biginteger1->array_size == 1 || p_biginteger2->array_size == 1){
+        return biginteger_mul(p_biginteger1, p_biginteger2);
+    }
+    int middle = p_biginteger1->array_size < p_biginteger2->array_size ? p_biginteger1->array_size : p_biginteger2->array_size;
+    middle >>= 1;
+
+    BigInteger* high1 = malloc(sizeof(BigInteger));
+    BigInteger* low1 = malloc(sizeof(BigInteger));
+    BigInteger* high2 = malloc(sizeof(BigInteger));
+    BigInteger* low2 = malloc(sizeof(BigInteger));
+    biginteger_split(p_biginteger1, middle, high1, low1);
+    biginteger_split(p_biginteger2, middle, high2, low2);
+
+    BigInteger* z0 = karatsuba(low1, low2);
+    BigInteger* tmp1 = biginteger_add(low1, high1);
+    BigInteger* tmp2 = biginteger_add(low2, high2);
+    BigInteger* z1 = karatsuba(tmp1, tmp2);
+    BigInteger* z2 = karatsuba(high1, high2);
+
+    // result = (z2 × 10 ^ (m2 × 2)) + ((z1 - z2 - z0) × 10 ^ m2) + z0
+    BigInteger* tmp3 = biginteger_left_shift(z2, middle << 1);
+    z2->is_positive = !z2->is_positive;
+    BigInteger* tmp4 = biginteger_add(z1, z2);
+    z0->is_positive = !z0->is_positive;
+    BigInteger* tmp5 = biginteger_add(tmp4, z0);
+    BigInteger* tmp6 = biginteger_left_shift(tmp5, middle);
+    z0->is_positive = !z0->is_positive;
+    z2->is_positive = !z2->is_positive;
+    BigInteger* tmp7 = biginteger_add(tmp3, tmp6);
+    BigInteger* result = biginteger_add(tmp7, z0);
+
+    biginteger_delete(tmp1);
+    biginteger_delete(tmp2);
+    biginteger_delete(tmp3);
+    biginteger_delete(tmp4);
+    biginteger_delete(tmp5);
+    biginteger_delete(tmp6);
+    biginteger_delete(tmp7);
+    biginteger_delete(high1);
+    biginteger_delete(low1);
+    biginteger_delete(high2);
+    biginteger_delete(low2);
+    biginteger_delete(z0);
+    biginteger_delete(z1);
+    biginteger_delete(z2);
+    return result;
+}
 
 
 int main(){
@@ -564,5 +631,61 @@ int main(){
     }
 
 
+    // char str1[10000];
+    // int split;
+    // printf("integer1: ");
+    // scanf("%s", str1);
+    // printf("split: ");
+    // scanf("%d", &split);
+    // BigInteger* p_biginteger1 = biginteger_from_string(str1, strlen(str1));
+    // biginteger_print_debug(p_biginteger1);
+    // biginteger_print(p_biginteger1);
+    // BigInteger* p_biginteger2 = malloc(sizeof(BigInteger));
+    // BigInteger* p_biginteger3 = malloc(sizeof(BigInteger));
+    // biginteger_split(p_biginteger1, split, p_biginteger2, p_biginteger3);
+    // printf("p_biginteger2->array_size %lu\n", p_biginteger2->array_size);
+    // printf("p_biginteger3->array_size %lu\n", p_biginteger3->array_size);
+    // biginteger_print_debug(p_biginteger2);
+    // biginteger_print_debug(p_biginteger3);
+    // biginteger_print(p_biginteger2);
+    // biginteger_print(p_biginteger3);
+    // biginteger_delete(p_biginteger1);
+    // biginteger_delete(p_biginteger2);
+    // biginteger_delete(p_biginteger3);
+
+    // char str1[10000];
+    // char str2[10000];
+    // printf("integer1: ");
+    // scanf("%s", str1);
+    // printf("integer2: ");
+    // scanf("%s", str2);
+    // BigInteger* p_biginteger1 = biginteger_from_string(str1, strlen(str1));
+    // BigInteger* p_biginteger2 = biginteger_from_string(str2, strlen(str2));
+    // biginteger_print_debug(p_biginteger1);
+    // biginteger_print(p_biginteger1);
+    // biginteger_print_debug(p_biginteger2);
+    // biginteger_print(p_biginteger2);
+    // BigInteger* p_biginteger3 = biginteger_mul(p_biginteger1, p_biginteger2);
+    // BigInteger* p_biginteger4 = karatsuba(p_biginteger1, p_biginteger2);
+    // biginteger_print_debug(p_biginteger3);
+    // biginteger_print(p_biginteger3);
+    // biginteger_print_debug(p_biginteger4);
+    // biginteger_print(p_biginteger4);
+    // biginteger_delete(p_biginteger1);
+    // biginteger_delete(p_biginteger2);
+    // biginteger_delete(p_biginteger3);
+    // biginteger_delete(p_biginteger4);
+
+    // char str1[10000];
+    // char str2[10000];
+    // while(scanf("%s %s", str1, str2) == 2){
+    //     BigInteger* p_biginteger1 = biginteger_from_string(str1, strlen(str1));
+    //     BigInteger* p_biginteger2 = biginteger_from_string(str2, strlen(str2));
+    //     BigInteger* p_biginteger3 = karatsuba(p_biginteger1, p_biginteger2);
+    //     biginteger_print(p_biginteger3);
+    //     biginteger_delete(p_biginteger1);
+    //     biginteger_delete(p_biginteger2);
+    //     biginteger_delete(p_biginteger3);
+    // }
     return 0;
 }
