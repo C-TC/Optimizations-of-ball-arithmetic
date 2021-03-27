351227d00730239a0c7cc74d14dc230a41633731
yunxin <martin.yunxin.sun@gmail.com>
2021-03-27 19:37:52 +0100

    add float type

---
+++ src/big_float.h
@@ -0,0 +1,11 @@
+#ifndef BIG_FLOAT_H
+#define BIG_FLOAT_H
+#include "big_integer.h"
+
+typedef struct BigFloat
+{
+    BigInteger *mantissa;
+    long long power;
+} BigFloat;
+
+#endif /* BIG_FLOAT_H */
