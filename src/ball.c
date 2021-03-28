4f0b62c391fc2d815f569e7f7803dcbf0f9f93a8
yunxin <martin.yunxin.sun@gmail.com>
2021-03-28 17:58:23 +0200

    add infras for ball

---
+++ src/ball.c
@@ -0,0 +1,13 @@
+#include "ball.h"
+Ball *ball_add(Ball *lo, Ball *ro, Ball *res) {
+    if (!res) res = malloc(sizeof(Ball));
+    return res;
+}
+Ball *ball_multiply(Ball *lo, Ball *ro, Ball *res) {
+    if (!res) res = malloc(sizeof(Ball));
+    return res;
+}
+Ball *ball_div(Ball *lo, Ball *ro, Ball *res) {
+    if (!res) res = malloc(sizeof(Ball));
+    return res;
+}
