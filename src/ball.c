7b9229fb10043bf77f07af74b4df248734c6dc4f
yunxin <martin.yunxin.sun@gmail.com>
2021-03-29 10:07:05 +0200

    add ball add

--- src/ball.c
+++ src/ball.c
@@ -1,6 +1,9 @@
 #include "ball.h"
+#include "bigfloat.h"
 Ball *ball_add(Ball *lo, Ball *ro, Ball *res) {
     if (!res) res = malloc(sizeof(Ball));
+    res->radius = lo->radius + ro->radius;
+    res->center = bigfloat_add(lo->center, ro->center, NULL);
     return res;
 }
 Ball *ball_multiply(Ball *lo, Ball *ro, Ball *res) {
