de3cd53e75ca27194b72701d92f003678e7a849a
Ran Liao <raliao@student.ethz.ch>
2021-05-03 19:44:40 +0200

    runtime plot

--- test/timing_mul.c
+++ test/timing_mul.c
@@ -7,7 +7,7 @@
 #define MAX_FUNC 100
 #define REP_COUNT 1
 
-double rdtsc_big_integer_add(BigInteger left, BigInteger right){
+double rdtsc_big_integer_multiply(BigInteger left, BigInteger right){
     int i, num_runs = 1;
     myInt64 cycles;
     myInt64 start;
@@ -15,7 +15,7 @@ double rdtsc_big_integer_add(BigInteger left, BigInteger right){
     while(num_runs < (1 << 14)){
         start = start_tsc();
         for (i = 0; i < num_runs; ++i) {
-            big_integer_add(left, right);
+            big_integer_multiply(left, right);
         }
         cycles = stop_tsc(start);
         if(cycles >= CYCLES_REQUIRED) break;
@@ -24,13 +24,13 @@ double rdtsc_big_integer_add(BigInteger left, BigInteger right){
 
     start = start_tsc();
     for (i = 0; i < num_runs; ++i) {
-        big_integer_add(left, right);
+        big_integer_multiply(left, right);
     }
     cycles = stop_tsc(start);
     return (double)cycles / num_runs;
 }
 
-double rdtsc_big_integer_add_inplace_fixed_precision(BigInteger* left, BigInteger right, int precision){
+double rdtsc_big_integer_multiply_inplace_fixed_precision(BigInteger* left, BigInteger right, int precision){
     int i, num_runs = 1;
     myInt64 cycles;
     myInt64 start;
@@ -38,7 +38,7 @@ double rdtsc_big_integer_add_inplace_fixed_precision(BigInteger* left, BigIntege
     while(num_runs < (1 << 14)){
         start = start_tsc();
         for (i = 0; i < num_runs; ++i) {
-            big_integer_add_inplace_fixed_precision(left, right, precision);
+            big_integer_multiply_inplace_fixed_precision(left, right, precision);
         }
         cycles = stop_tsc(start);
         if(cycles >= CYCLES_REQUIRED) break;
@@ -47,7 +47,7 @@ double rdtsc_big_integer_add_inplace_fixed_precision(BigInteger* left, BigIntege
 
     start = start_tsc();
     for (i = 0; i < num_runs; ++i) {
-        big_integer_add_inplace_fixed_precision(left, right, precision);
+        big_integer_multiply_inplace_fixed_precision(left, right, precision);
     }
     cycles = stop_tsc(start);
     return (double)cycles / num_runs;
@@ -76,10 +76,10 @@ int main(){
         double cycles_add = 0;
         double cycles_add_precision = 0;
         for(int j=0;j<REP_COUNT;j++){
-            cycles_add += rdtsc_big_integer_add(left, right);
+            cycles_add += rdtsc_big_integer_multiply(left, right);
         }
         for(int j=0;j<REP_COUNT;j++){
-            cycles_add_precision += rdtsc_big_integer_add_inplace_fixed_precision(&left, right, n);
+            cycles_add_precision += rdtsc_big_integer_multiply_inplace_fixed_precision(&left, right, n);
         }
         printf("%d %.2lf %.2lf\n", n, cycles_add / REP_COUNT, cycles_add_precision / REP_COUNT);
     }
---
+++ test/timing_mul.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+def load_data(filename):
+    data_x = []
+    data_y1 = []
+    data_y2 = []
+    with open(filename, 'r') as fi:
+        for line in fi.readlines():
+            x, cycle1, cycle2 = line.strip().split(' ')
+            data_x.append(int(x))
+            data_y1.append(float(cycle1))
+            data_y2.append(float(cycle2))
+    return data_x, data_y1, data_y2
+
+if __name__ == '__main__':
+    data_x, data_y1, data_y2 = load_data('timing_mul.data')
+
+    plt.style.use('seaborn-darkgrid')
+    plt.figure(figsize=(12.8, 7.2))
+    line_add, = plt.plot(data_x, data_y1, marker='.', color='black', linewidth=1.5, alpha=0.7, label='big_integer_multiply')
+    line_precision, = plt.plot(data_x, data_y2, marker='^', color='darkred', linewidth=1.5, alpha=0.7, label='big_integer_multiply_inplace_fixed_precision')
+    plt.legend(handles=[line_add, line_precision], fontsize='x-large')
+
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xticks(data_x, [r'$2^{' + str(i) + r'}$' for i in range(3, 13)], fontsize=14)
+    plt.yticks(fontsize=14)
+
+    plt.title("Intel® Core™ i5-7360U CPU @ 2.30GHz\nL1: 32KB, L2: 256KB, L3: 4MB\nCompiler: clang 12.0.0", loc='left', fontsize=16, fontweight=1, color='black')
+    plt.xlabel("Input Size", fontsize=16)
+    plt.ylabel("Runtime [Cycle]", fontsize=16)
+    plt.savefig('timing_mul.pdf')
+
+    # plt.show()
