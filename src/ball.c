a3ed70026704f4b44175ca6b26b4fbc240726715
tiachen <tiachen@ethz.ch>
2021-05-04 22:36:24 +0200

    fix bug in ball arithmetic(abs value)

--- src/ball.c
+++ src/ball.c
@@ -2,6 +2,7 @@
 #include "ball_helper.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 // #include "tsc_x86.h"
 Ball ball_add(Ball lo, Ball ro) {
     Ball ans;
@@ -19,18 +20,18 @@ Ball ball_multiply(Ball lo, Ball ro) {
     Ball ans;
     ans.center = big_float_multiply(lo.center,ro.center);
     //ans.radius = dl*r + dr*l + l*r
-    ans.radius = big_float_to_double(lo.center) * ro.radius + big_float_to_double(ro.center) * lo.radius + lo.radius * ro.radius;
+    ans.radius = fabs(big_float_to_double(lo.center)) * ro.radius + fabs(big_float_to_double(ro.center)) * lo.radius + lo.radius * ro.radius;
     return ans;
 }
 Ball ball_div(Ball lo, Ball ro) {
     Ball ans;
     //TODO: what about ro = 0? how to define infinity in bigfloat?
     ans.center = big_float_div(lo.center,ro.center);
-    double anscentervalue = big_float_to_double(ans.center);
-    double rovalue = big_float_to_double(ro.center);
+    double ans_center_abs_value = fabs(big_float_to_double(ans.center));
+    double ro_abs_value = fabs(big_float_to_double(ro.center));
     // TODO: optimize this
     // ans.radius = l/r + l*dr/r^2 + dl/r + dl*dr/r^2
-    ans.radius = anscentervalue + anscentervalue / big_float_to_double(ro.center) * ro.radius + lo.radius / rovalue + lo.radius * ro.radius / rovalue / rovalue;
+    ans.radius = ans_center_abs_value + ans_center_abs_value / fabs(big_float_to_double(ro.center)) * ro.radius + lo.radius / ro_abs_value + lo.radius * ro.radius / ro_abs_value / ro_abs_value;
     return ans;
 }
 
